04d3ef0bc1874e6bb2f8d914144234de3a576634
hswick <hdswick@gmail.com>
2018-04-10 20:30:36 -0500

    Grabbing contract at address now

--- client/cli/session.js
+++ client/cli/session.js
@@ -1,13 +1,34 @@
 const fs = require('fs')
+const contract = require('truffle-contract')
+const Web3 = require('web3')
 
-module.exports = (rl, config, network) => {
+
+module.exports = async (rl, config, network) => {
 	if (!config['networks'][network]) {
 		console.error(network + " is not a configured network. Enter `network` to see which networks are configured.")
 	} else {
-		let networkConfig = config['networks'][network]
 
-		let incentiveLayerContracts = JSON.parse(fs.readFileSync(networkConfig["incentive-layer"] + "/" + network + ".json"))
-		let disputeResolutionLayerContracts = JSON.parse(fs.readFileSync(networkConfig['dispute-resolution-layer'] + "/" + network + ".json"))
+		//initialize provider
+		const web3 = new Web3.providers.HttpProvider("http://localhost:8545")
 
+		const networkConfig = config['networks'][network]
+
+		const incentiveLayerContracts = JSON.parse(fs.readFileSync(networkConfig["incentive-layer"] + "/" + network + ".json"))
+		const disputeResolutionLayerContracts = JSON.parse(fs.readFileSync(networkConfig['dispute-resolution-layer'] + "/" + network + ".json"))
+
+		let incentiveLayer = contract({abi: incentiveLayerContracts['TaskExchange'].abi})
+		incentiveLayer.setProvider(web3)
+
+//dirty hack for web3@1.0.0 support for localhost testrpc, see https://github.com/trufflesuite/truffle-contract/issues/56#issuecomment-331084530
+if (typeof incentiveLayer.currentProvider.sendAsync !== "function") {
+  incentiveLayer.currentProvider.sendAsync = function() {
+    return incentiveLayer.currentProvider.send.apply(
+      incentiveLayer.currentProvider, arguments
+    )
+  }
+}
+
+		console.log(await incentiveLayer.at(incentiveLayerContracts['TaskExchange'].address))
 	}
+	rl.prompt()
 }
