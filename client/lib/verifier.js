d92985b839298f83c3bfe4d8ee648f89a997817a
hswick <hdswick@gmail.com>
2018-04-12 15:46:48 -0500

    verifier is watching for solutions now

--- client/lib/verifier.js
+++ client/lib/verifier.js
@@ -15,7 +15,34 @@ module.exports = async (session, args) => {
       await depositsHelper(session, account, minDeposit)
             
 			//start monitoring for solutions
-			
+			let solutionCommittedEvent = session.contracts.incentiveLayer.SolutionCommitted()
+
+			new Promise(async (resolve, reject) => {
+				solutionCommittedEvent.watch(async (error, result) => {
+					if (result) {
+						let taskID = result.args.taskID.toNumber()
+						let taskMinDeposit = result.args.minDeposit.toNumber()
+						let solverSolutionHash = result.args.solutionHash
+
+						if (taskMinDeposit < minDeposit) {
+							let taskData = await session.contracts.incentiveLayer.getTaskData.call(taskID)
+
+							let program = session.web3.utils.hexToBytes(taskData[0]).map((n) => {
+								return util.bufferToHex(util.setLengthLeft(n, 32))
+							})
+
+							let output = await session.contracts.computationLayer.runSteps.call(program, taskData[1].toNumber())
+
+							let solutionHash = output[1]
+
+							if (solutionHash != solverSolutionHash) {
+								await session.contracts.incentiveLayer.commitChallenge(taskID, {from: account})
+							}
+						}
+					}
+				})
+			})
+
 		}
 
 	}
