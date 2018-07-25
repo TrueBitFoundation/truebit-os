27276d788d567ce4314e60598a9d5f8c5da6a645
hswick <hdswick@gmail.com>
2018-07-25 18:44:45 -0400

    Added yes to end of opam init

--- wasm-client/install.sh
+++ wasm-client/install.sh
@@ -15,7 +15,7 @@ sudo apt-get update
 sudo apt-get install -y wget gcc m4 pkg-config zlib1g-dev
 sudo wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin/
 
-yes | opam init --comp=4.06.1
+opam init --comp=4.06.1 -y
 
 eval $(opam config env)
 opam install cryptokit yojson
