324213a45976c295a9d1923f6f7cbfc71b882a5f
Sami Mäkelä <mrsmkl@gmail.com>
2018-09-04 09:22:31 +0300

    tests with no challengers are ok now

--- test/os-wasm-ipfs.js
+++ test/os-wasm-ipfs.js
@@ -94,6 +94,8 @@ describe('Truebit OS WASM IPFS', async function() {
 	    await timeout(5000)
 	    await mineBlocks(os.web3, 110)
 	    await timeout(5000)
+	    await mineBlocks(os.web3, 110)
+	    await timeout(5000)
 	    
 	    let tasks = os.taskGiver.getTasks()
 	    //taskID = Object.keys(tasks)[0]
