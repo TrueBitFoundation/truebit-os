ddb97e63ece0a1b58a2e723fe6e66b98228c3a6c
hswick <hdswick@gmail.com>
2018-11-07 18:03:59 -0600

    Added gmp lib, wasn't available on latest ubuntu release

--- wasm-client/install.sh
+++ wasm-client/install.sh
@@ -6,7 +6,7 @@ git submodule init
 git submodule update
 
 sudo apt-get update
-sudo apt-get install -y wget gcc m4 pkg-config zlib1g-dev
+sudo apt-get install -y wget gcc m4 pkg-config zlib1g-dev libgmp3-dev
 sudo wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin/
 
 opam init --comp=4.06.1 -y
