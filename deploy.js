ee063c2977ad5dfd009ed9c33afce4bc3602cdb2
hswick <hdswick@gmail.com>
2018-12-18 15:03:24 -0600

    Added test and prod jackpot manager to deploy script

--- deploy.js
+++ deploy.js
@@ -45,6 +45,16 @@ async function deploy() {
 
     let tru = await deployContract('TRU', {from: accounts[0], gas: 2000000})
     let exchangeRateOracle = await deployContract('ExchangeRateOracle', {from: accounts[0], gas: 1000000})
+
+    let jackpotManager
+
+    if (process.env.NODE_ENV == 'production') {
+	jackpotManager = await deployContract('JackpotManager', {from: accounts[0], gas: 500000}, [tru._address])
+    } else {
+	jackpotManager = await deployContract('NeverJackpotManager', {from: accounts[0], gas: 500000}, [tru._address])
+	//jackpotManager = await deployContract('AlwaysJackpotManager', {from: accounts[0], gas: 500000}, [tru._address])	
+    }
+    
     let incentiveLayer = await deployContract('IncentiveLayer', {from: accounts[0], gas: 5200000}, [tru._address, exchangeRateOracle._address, interactive._address, fileSystem._address])
     
     // tru.methods.transferOwnership(incentiveLayer._address).send({from: accounts[0], gas: 1000000})
