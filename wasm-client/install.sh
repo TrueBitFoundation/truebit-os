8ff258815835ab4ff9f6c3a053112e2e81fccaf7
Sami Mäkelä <mrsmkl@gmail.com>
2019-02-26 11:19:25 -0800

    testing travis

--- wasm-client/install.sh
+++ wasm-client/install.sh
@@ -1,9 +1,9 @@
 
 # This script installs the offchain interpreter
 
-apt-get update
-apt-get install -y wget gcc m4 pkg-config zlib1g-dev libffi-dev libgmp3-dev unzip
-wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin/
+sudo apt-get update
+sudo apt-get install -y wget gcc m4 pkg-config zlib1g-dev libffi-dev libgmp3-dev unzip
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sudo sh -s /usr/local/bin/
 
 opam init --comp=4.06.1 -y
 
