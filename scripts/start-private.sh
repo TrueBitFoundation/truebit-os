b4f0f659064db1d114b288412e64961b11761948
Sami Mäkelä <mrsmkl@gmail.com>
2019-03-08 02:24:27 -0800

    added new setup script

---
+++ scripts/start-private.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Starting up Ganache and IPFS
+
+ganache-cli -h 0.0.0.0 > ~/ganache_log 2>&1 &
+ipfs daemon  > ~/ipfs_log 2>&1 &
+
+echo "Logs should be at ~/ganache_log and ~/ipfs_log"
+
+sleep 5
+
+npm run deploy
+
+node cli/index.js -c "start solve" --batch
+
